bf44125bdffe702f1e6f1e6f8fe72e2f4044df30
Kechko Ilya <kechko.ilya@gmail.comgit clone https://github.com/lyakechko/MyFirst.gitgit config --global user.email kechko.ilya@gmail.com>
2022-10-19 19:26:28 +0300

    add all files

---
+++ app/src/main/java/com/example/mykinopoisk/presentation/ui/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mykinopoisk.presentation.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import com.example.mykinopoisk.R
+
+class MainActivity : AppCompatActivity(R.layout.activity_main)
